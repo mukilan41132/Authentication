3700f3171e56668a51c4cd863710d14a846c2927
Mukilan <rmukilan2002@gmail.com>
2024-09-08 21:53:25 +0530

    Authentication learning

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const path = require('path');
 
+const  mongodbStore  = require('connect-mongodb-session');
+
 const express = require('express');
 
+const session = require('express-session');
+
+var MongoDBStore = mongodbStore(session);
+
 const db = require('./data/database');
+
 const demoRoutes = require('./routes/demo');
 
+
 const app = express();
+
+var store = new MongoDBStore({
+  uri: 'mongodb://localhost:27017',
+  databaseName: 'auth-demo',
+  collection: 'mySessions'
+});
+
+app.use(session({
+  secret: 'super-secret',
+  resave: false,
+  saveUninitialized: false,
+  store: store,
+}))
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -15,7 +36,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(demoRoutes);
 
-app.use(function(error, req, res, next) {
+app.use(function (error, req, res, next) {
   res.render('500');
 })
 
--- routes/demo.js
+++ routes/demo.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const db = require('../data/database');
 
+
 const router = express.Router();
 
 router.get('/', function (req, res) {
@@ -9,7 +10,19 @@ router.get('/', function (req, res) {
 });
 
 router.get('/signup', function (req, res) {
-  res.render('signup');
+
+  let sessionInputData = req.session.inputData;
+  console.log("15",sessionInputData)
+  if (!sessionInputData) {
+    sessionInputData = {
+      hasError: false,
+      email: '',
+      confirmedEmail: '',
+      password: ''
+    }
+  }
+  req.session.inputData = null;
+  res.render('signup', { inputData: sessionInputData });
 });
 
 router.get('/login', function (req, res) {
@@ -22,9 +35,23 @@ router.post('/signup', async function (req, res) {
   const confirmedEmail = userData['confirm-email'];
   const enteredPassword = userData.password;
 
-  if (!enteremail || !confirmedEmail || enteredPassword.trim() < 6 || enteremail !== confirmedEmail || !enteremail.includes('@')) {
-    console.log('Incorrect data');
-    return res.redirect('/signup')
+  if (!enteremail ||
+    !confirmedEmail ||
+    enteredPassword.trim() < 6 ||
+    enteremail !== confirmedEmail ||
+    !enteremail.includes('@')) {
+
+    req.session.inputData = {
+      hasError: true,
+      message: 'Invalid input - pleace check your data',
+      email: enteremail,
+      confirmedEmail: confirmedEmail,
+      password: enteredPassword
+    }
+    req.session.save(function () {
+      return res.render('/signup')
+    })
+
   }
 
   const existingUser = await db.getDb().collection('users').findOne({
@@ -63,14 +90,26 @@ router.post('/login', async function (req, res) {
     console.log('Password IN Correct');
     return res.redirect('/login');
   }
-  console.log('unAutherized user !!');
-  res.redirect('/admin')
+
+
+  req.session.user = { id: existingUser._id.toString(), email: existingUser.email }
+  req.session.isAuthenticated = true;
+  req.session.save(function () {
+    res.redirect('/admin')
+  })
 });
 
 router.get('/admin', function (req, res) {
+  if (!req.session.isAuthenticated) {
+    return res.status(401).render('401');
+  }
   res.render('admin');
 });
 
-router.post('/logout', function (req, res) { });
+router.post('/logout', function (req, res) {
+  req.session.user = null
+  req.session.isAuthenticated = true;
+  res.redirect('/');
+});
 
 module.exports = router;
